001ea4f54f1b2a0c6541b23f5ee14fba2bc3d0d7
Ecovision Analytics <solutions@ecovisionanalytics.com>
2025-08-08 21:33:59 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bike-builder-checklist",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "streamlit==1.26.0",
+        "pandas==2.0.3",
+        "pillow==10.0.0",
+    ],
+)
